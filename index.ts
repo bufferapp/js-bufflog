d15c0e85a717629a29a65c413f8ccefccdf7be63
Eric Khun <enregistrements@gmail.com>
2020-02-24 14:46:32 +0000

    cleanup

--- index.ts
+++ index.ts
@@ -19,17 +19,17 @@ logger.critical('hello critical', {"some":"stuff"});
 
 const app = express();
 
-// app.listen(4000, () => {
-//     console.log(`Server is listening on port 4000`);
-// });
+app.listen(4000, () => {
+    console.log(`Server is listening on port 4000`);
+});
 
-// app.get('/', (req, res) =>  {
-//     var logger = new BuffLog();
-//     logger.notice("Notice log via endpoint");
-//     logger.info('hello info');
-//     logger.debug('hello debug');
-//     logger.notice('hello notice');
-//     logger.warning('hello warning');
-//     logger.error('hello error');
-//     logger.critical('hello critical');
-// });
+app.get('/', (req, res) =>  {
+    var logger = new BuffLog();
+    logger.notice("Notice log via endpoint");
+    logger.info('hello info');
+    logger.debug('hello debug');
+    logger.notice('hello notice');
+    logger.warning('hello warning');
+    logger.error('hello error');
+    logger.critical('hello critical');
+});
